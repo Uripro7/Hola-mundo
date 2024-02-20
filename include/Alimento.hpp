6647326953e2876423baa65cabef81a1bd1ceb16
Uripro7 <160290059+Uripro7@users.noreply.github.com>
2024-02-20 02:07:01 +0000

    version 1

---
+++ include/Alimento.hpp
@@ -0,0 +1,14 @@
+#pragma once
+class Alimento
+{
+private:
+    int Energia;
+public:
+    Alimento( int Energia ) {
+        this->Energia = Energia;
+    }
+    ~Alimento() {}
+    int ExtraerEnergia(){
+        return Energia; 
+    }
+};
--- main.cpp
+++ main.cpp
@@ -1,9 +1,21 @@
 #include <iostream>
 using namespace std;
 
+#include <Mascota.hpp>
+#include <Alimento.hpp>
+
 int main(int argc, char const *argv[])
 {
-    cout<<"hola"<<"22310173"<<endl;
-    /* code */
+    Mascota m1;
+    Mascota m2;
+    Mascota m3;
+    Alimento alimento(5);
+    m1.Comer(alimento);
+    m2.Comer(alimento);
+    m3.Comer(alimento);
+
+    cout<<"Mascota"<<"m1.LeerEnergia()"<<endl;
+
+
     return 0;
 };
